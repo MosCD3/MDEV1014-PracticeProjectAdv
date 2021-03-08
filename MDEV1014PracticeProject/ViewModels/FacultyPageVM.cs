0ea3c0a006ad18f56a85a0d291b4b92d8ff88137
Mostafa Gamal <moscd3@gmail.com>
2021-03-07 22:59:24 -0500

    Lecture 7 CollectionView

--- MDEV1014PracticeProject/ViewModels/FacultyPageVM.cs
+++ MDEV1014PracticeProject/ViewModels/FacultyPageVM.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using MDEV1014PracticeProject.Models;
 using Xamarin.Forms;
 
 namespace MDEV1014PracticeProject.ViewModels
@@ -16,6 +19,13 @@ namespace MDEV1014PracticeProject.ViewModels
             set { SetProperty(ref _PageTitle, value); }
         }
 
+        public ObservableCollection<Faculty> _itemslist = new ObservableCollection<Faculty>();
+        public ObservableCollection<Faculty> ItemsList
+        {
+            get { return _itemslist; }
+            set { SetProperty(ref _itemslist, value); }
+        }
+
         public ICommand ChangeTitleCommand { get; set; }
 
         public FacultyPageVM()
@@ -23,18 +33,35 @@ namespace MDEV1014PracticeProject.ViewModels
             PageTitle = "Faculty List";
             ChangeTitleCommand = new Command(() =>
             {
-                Console.WriteLine("inotify method");
                 PageTitle = "Menu Items";
             });
+
+            LoadFacultyAsync();
 
         }
 
         void ChangeTitleAction() {
         }
 
-        void LoadFacultyAsync() {
+        void  LoadFacultyAsync() {
             //3 sec feh API
-            //FacultyList = "sdfjskdf"
+            //Load faculty information from API
+            var itemsResult = new ObservableCollection<Faculty>();
+            itemsResult.Add(new Faculty { Name = "Moos", Department = "IT", Email = "moos@college.ca" , AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Jessics", Department = "IT", Email = "jess@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Mark", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "David", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Anyname", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            itemsResult.Add(new Faculty { Name = "Rob", Department = "IT", Email = "rob@college.ca", AvatarUrl = "customer.png" });
+            //loading from server too 3 sec
+            Task.Delay(3000);
+            ItemsList = itemsResult;
+
         }
     }
 }
--- MDEV1014PracticeProject/Views/FacultyPage.xaml.cs
+++ MDEV1014PracticeProject/Views/FacultyPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using MDEV1014PracticeProject.Models;
 using MDEV1014PracticeProject.ViewModels;
 using Xamarin.Forms;
 
@@ -15,6 +18,26 @@ namespace MDEV1014PracticeProject.Views
 
         void collectionList_SelectionChanged(System.Object sender, Xamarin.Forms.SelectionChangedEventArgs e)
         {
+            if (collectionList.SelectedItem == null) return;
+
+            var userObject = (e.CurrentSelection.FirstOrDefault() as Faculty);
+
+            if (userObject != null)
+            {
+
+                string current = userObject.Name;
+
+                Debug.WriteLine($"current selected faculty:{current}");
+
+                var newPage = new ContactDetailsPage(new User { Name = current });
+                //var newPage = new ContactDetailsPage(user: userObject);
+                Navigation.PushAsync(newPage);
+
+            }
+
+
+
+            collectionList.SelectedItem = null;
         }
 
         void Button_Clicked(System.Object sender, System.EventArgs e)
